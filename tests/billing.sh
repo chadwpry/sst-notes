e54eafdfd4eb30b17c1fc0311dcf74c57d64619e
Chad W Pry <chad.pry@gmail.com>
2024-01-05 01:48:13 -0600

    Adding a billing API

---
+++ tests/billing.sh
@@ -0,0 +1,12 @@
+pnpm dlx aws-api-gateway-cli-test \
+  --username='chad.pry@gmail.com' \
+  --password='SSTdev123!' \
+  --user-pool-id='us-east-1_2CpFnwRlU' \
+  --app-client-id='3pg001444dd66aj0oh8fu08pem' \
+  --cognito-region='us-east-1' \
+  --identity-pool-id='us-east-1:eb035a52-b45b-477e-99a0-3220669156d4' \
+  --invoke-url='https://zetq1k6w1j.execute-api.us-east-1.amazonaws.com' \
+  --api-gateway-region='us-east-1' \
+  --path-template='/billing' \
+  --method='POST' \
+  --body='{"source":"tok_visa","storage":21}'
